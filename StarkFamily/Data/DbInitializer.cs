ac1913649a72ff6bcf6fbf1c9e0b423718c813e4
mlk.ia <mlk.2plus@gmail.com>
2021-12-09 10:46:21 +0100

    Fix DB Initializer

--- StarkFamily/Data/DbInitializer.cs
+++ StarkFamily/Data/DbInitializer.cs
@@ -20,20 +20,20 @@ namespace StarkFamily.Data
 
             var persons = new Person[]
             {
-            new Person{Id=1, FirstName="Rickard",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"), DeathDate=DateTime.Parse("1986-9-21"),},
-            new Person{Id=2, FirstName="Mother",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"), DeathDate=DateTime.Parse("1986-9-21")},
-            new Person{Id=3, FirstName="Brandon",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"),DeathDate=DateTime.Parse("1986-9-21"),FatherId=1,/*MotherId=2*/},
+            new Person{FirstName="Rickard",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"), DeathDate=DateTime.Parse("1986-9-21"),},
+            new Person{FirstName="Mother",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"), DeathDate=DateTime.Parse("1986-9-21")},
+            new Person{FirstName="Brandon",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"),DeathDate=DateTime.Parse("1986-9-21"),FatherId=1,},
             new Person{FirstName="Eddard",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"), DeathDate=DateTime.Parse("1986-9-21"),FatherId=1},
             new Person{FirstName="Benjen",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"), DeathDate=DateTime.Parse("1986-9-21"),FatherId=1},
-            new Person{Id=5, FirstName="Lyanna",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"), DeathDate=DateTime.Parse("1986-9-21"),FatherId=1},
-            new Person{Id=4, FirstName="Catelyn",LastName="Tulle", BirthDate=DateTime.Parse("1936-03-17"),DeathDate=DateTime.Parse("1986-9-21")},
-            new Person{Id=6, FirstName = "Rhaenys", LastName = "Tagaryen", BirthDate = DateTime.Parse("1936-03-17"), DeathDate = DateTime.Parse("1986-9-21"), FatherId =3},
-            new Person{FirstName="Robb",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"),DeathDate=DateTime.Parse("1986-9-21"),FatherId=3 },
-            new Person{FirstName="Snas",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"), DeathDate=DateTime.Parse("1986-9-21"),FatherId=3,/*MotherId=4*/},
-            new Person{FirstName="Arya",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"), DeathDate=DateTime.Parse("1986-9-21"),FatherId=3 },
-            new Person{FirstName="Bran",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"), DeathDate=DateTime.Parse("1986-9-21"),FatherId=3 },
-            new Person{FirstName="Rickon",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"), DeathDate=DateTime.Parse("1986-9-21"),FatherId=3 },
-            new Person{FirstName="Jon",LastName="Snow", BirthDate=DateTime.Parse("1936-03-17"), DeathDate=DateTime.Parse("1986-9-21"),FatherId=6,/*MotherId=5*/ },
+            new Person{FirstName="Lyanna",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"), DeathDate=DateTime.Parse("1986-9-21"),FatherId=1},
+            new Person{FirstName="Catelyn",LastName="Tulle", BirthDate=DateTime.Parse("1936-03-17"),DeathDate=DateTime.Parse("1986-9-21")},
+            new Person{FirstName ="Rhaenys",LastName ="Tagaryen",BirthDate = DateTime.Parse("1936-03-17"),DeathDate = DateTime.Parse("1986-9-21")},
+            new Person{FirstName="Robb",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"),DeathDate=DateTime.Parse("1986-9-21"),FatherId=4 },
+            new Person{FirstName="Snas",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"), DeathDate=DateTime.Parse("1986-9-21"),FatherId=4,},
+            new Person{FirstName="Arya",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"), DeathDate=DateTime.Parse("1986-9-21"),FatherId=4 },
+            new Person{FirstName="Bran",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"), DeathDate=DateTime.Parse("1986-9-21"),FatherId=4 },
+            new Person{FirstName="Rickon",LastName="Stark", BirthDate=DateTime.Parse("1936-03-17"), DeathDate=DateTime.Parse("1986-9-21"),FatherId=4 },
+            new Person{FirstName="Jon",LastName="Snow", BirthDate=DateTime.Parse("1936-03-17"), DeathDate=DateTime.Parse("1986-9-21"),FatherId=8,},
             };
             foreach (Person s in persons)
             {
